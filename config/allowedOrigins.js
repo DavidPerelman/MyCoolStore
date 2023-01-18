ac9b23a95aafb3dec510b102e6c992ee9481e777
David Perelman <dperelman3@gmail.com>
2023-01-18 14:27:45 +0200

    allow Origin: https://david-cool-store.netlify.app/

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3000',
   'https://prussian-blue-xerus-cuff.cyclic.app/',
+  'https://david-cool-store.netlify.app/',
 ];
 
 module.exports = allowedOrigins;
