fe4479e0b04f91a54d0c7d01181563db179c233e
David Perelman <dperelman3@gmail.com>
2023-01-18 18:28:09 +0200

    add React-Bootstrap Navbar

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import classes from './Header.module.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
-        <Link to='/'>MyCoolStore</Link>
+        <Navbar className={classes.header}>
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>MyCoolStore</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <Outlet />
     </>
