910a33e7837f8975ba075a96b3aec4f5f0f78bac
David Perelman <dperelman3@gmail.com>
2023-01-18 19:33:24 +0200

    setup Rating component

--- client/src/components/Layout/ProductCard/ProductCard.js
+++ client/src/components/Layout/ProductCard/ProductCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Rating from '../../UI/Rating/Rating';
 
 const ProductCard = ({ product }) => {
   return (
@@ -16,7 +17,8 @@ const ProductCard = ({ product }) => {
       <Card.Body>
         <Link to={`/product/${product.id}`}>
           <Card.Title>{product.title}</Card.Title>
-        </Link>
+        </Link>{' '}
+        <Rating rating={product.rating} />
         <Card.Text>${product.price}</Card.Text>
         <Button>Add to cart</Button>
       </Card.Body>
