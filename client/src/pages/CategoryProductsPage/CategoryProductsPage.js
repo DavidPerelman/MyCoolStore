d427517538a2674258b42eb4874710c8062f3e6e
David Perelman <dperelman3@gmail.com>
2023-01-19 19:36:05 +0200

    CategoryProductsPage

--- client/src/pages/CategoryProductsPage/CategoryProductsPage.js
+++ client/src/pages/CategoryProductsPage/CategoryProductsPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import classes from './CategoryProductsPage.module.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useCategoryTitleQuery } from '../../hooks/useCategoriesQuery';
+import { useContainerProductsQuery } from '../../hooks/useProductsQuery';
 
 const CategoryProductsPage = () => {
-  return <div>CategoryProductsPage</div>;
+  const { categoryId } = useParams();
+  const navigate = useNavigate();
+
+  const { data: categoryName } = useCategoryTitleQuery(categoryId);
+  const { data: products } = useContainerProductsQuery(categoryId);
+
+  console.log(products);
+
+  return <h1>{categoryName}</h1>;
 };
 
 export default CategoryProductsPage;
